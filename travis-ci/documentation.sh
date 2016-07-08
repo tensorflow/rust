2d0ded33cf615e05b851c7cd533db782b4c351dd
Adam Crume <acrume@google.com>
2016-07-07 20:40:50 -0700

    Add Travis CI configuration

---
+++ travis-ci/documentation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ev
+
+[ ! -z "${RUSTDOC_VERSION}" ] && [ "${TRAVIS_RUST_VERSION}" != "${RUSTDOC_VERSION}" ] && exit
+[ -z "${RUSTDOC_VERSION}" ] && [ "${TRAVIS_RUST_VERSION}" != "nightly" ] && exit
+[ "${TRAVIS_BRANCH}" != "master" ] && exit
+
+git config user.name "Travis CI"
+git config user.email ""
+
+if [ -z "${CRATE_NAME}" ]; then
+  CRATE_NAME=$(echo ${TRAVIS_REPO_SLUG} | cut -d '/' -f 2 | sed 's/-/_/g')
+fi
+
+URL="${CRATE_NAME}/index.html"
+
+HTML="<!DOCTYPE html>
+<link rel='canonical' href='${URL}'>
+<meta http-equiv='refresh' content='0; url=${URL}'>
+<script>window.location='${URL}'</script>"
+
+cargo doc --features tensorflow_unstable ${CARGO_FLAGS}
+echo "${HTML}" > target/doc/index.html
+
+export PYTHONUSERBASE="${HOME}/.local"
+pip install ghp-import --user ${USER}
+${PYTHONUSERBASE}/bin/ghp-import -m 'Update the documentation' -n target/doc
+
+git push -qf https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
---
+++ travis-ci/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/0.3.0/bazel-0.3.0-installer-linux-x86_64.sh
+
+# Copied from https://raw.githubusercontent.com/michaelklishin/jdk_switcher/master/jdk_switcher.sh
+source jdk_switcher.sh
+jdk_switcher use oraclejdk8
+
+set -e
+
+curl -o bazel.sh -L ${BAZEL_URL}
+chmod +x bazel.sh
+./bazel.sh --user
