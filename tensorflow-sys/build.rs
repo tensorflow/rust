fce5caf34bdaa888f103c742aa4773b88f9599b9
Adam Crume <acrume@google.com>
2016-09-28 18:40:28 -0700

    Add logging to tensorflow-sys/build.rs and avoid copying over existing files

--- tensorflow-sys/build.rs
+++ tensorflow-sys/build.rs
@@ -15,16 +15,37 @@ const VERSION: &'static str = "0.10.0";
 
 macro_rules! get(($name:expr) => (ok!(env::var($name))));
 macro_rules! ok(($expression:expr) => ($expression.unwrap()));
+macro_rules! log {
+    ($fmt:expr) => (println!(concat!("libtensorflow-sys/build.rs:{}: ", $fmt), line!()));
+    ($fmt:expr, $($arg:tt)*) => (println!(concat!("libtensorflow-sys/build.rs:{}: ", $fmt), line!(), $($arg)*));
+}
+macro_rules! log_var(($var:ident) => (log!(concat!(stringify!($var), " = {:?}"), $var)));
 
 fn main() {
     if pkg_config::find_library(LIBRARY).is_ok() {
+        log!("Returning early because {} was already found", LIBRARY);
         return;
     }
 
-    let target_path = &TARGET.replace(":", "/");
     let output = PathBuf::from(&get!("OUT_DIR"));
-    if !output.join(target_path).exists() {
-        let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join(format!("target/source-{}", VERSION));
+    log_var!(output);
+    let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join(format!("target/source-{}", VERSION));
+    log_var!(source);
+    let lib_dir = output.join(format!("lib-{}", VERSION));
+    log_var!(lib_dir);
+    if lib_dir.exists() {
+        log!("Directory {:?} already exists", lib_dir);
+    } else {
+        log!("Creating directory {:?}", lib_dir);
+        fs::create_dir(lib_dir.clone()).unwrap();
+    }
+    let library_path = lib_dir.join(format!("lib{}.so", LIBRARY));
+    log_var!(library_path);
+    if library_path.exists() {
+        log!("{:?} already exists, not building", library_path);
+    } else {
+        let target_path = &TARGET.replace(":", "/");
+        log_var!(target_path);
         if !Path::new(&source.join(".git")).exists() {
             run("git", |command| command.arg("clone")
                                         .arg(format!("--branch=v{}", VERSION))
@@ -39,20 +60,13 @@ fn main() {
                                       .arg(format!("--jobs={}", get!("NUM_JOBS")))
                                       .arg("--compilation_mode=opt")
                                       .arg(TARGET));
-        let library_path = output.join(format!("{}.so", LIBRARY));
-        if library_path.exists() {
-            // The file gets copied as readonly, so we get a permission error when overwriting an existing copy.
-            // The simplest way to solve it is to delete the old file, if it exists.
-            if let Err(e) = fs::remove_file(&library_path) {
-                println!("cargo:warning=Unable to delete old copy of {}: {}",
-                         library_path.to_string_lossy(), e)
-            }
-        }
-        fs::copy(source.join("bazel-bin").join(target_path), library_path).unwrap();
+        let target_bazel_bin = source.join("bazel-bin").join(target_path);
+        log!("Copying {:?} to {:?}", target_bazel_bin, library_path);
+        fs::copy(target_bazel_bin, library_path).unwrap();
     }
 
     println!("cargo:rustc-link-lib=dylib={}", LIBRARY);
-    println!("cargo:rustc-link-search={}", output.display());
+    println!("cargo:rustc-link-search={}", lib_dir.display());
 }
 
 // Patches in https://github.com/tensorflow/tensorflow/commit/d03f2545ecc3012e6c941a3a1e957f7d7f8d5040
@@ -60,10 +74,14 @@ fn main() {
 // TODO(acrume): Remove once we're on a non-broken version.
 fn patch_build_file(source: &PathBuf) -> Result<(), Box<Error>> {
     let build_file = source.join("tensorflow/BUILD");
+    log!("Checking build file {:?}", build_file);
     let mut content = String::new();
-    let mut file = try!(OpenOptions::new().read(true).append(true).open(build_file));
+    let mut file = try!(OpenOptions::new().read(true).append(true).open(build_file.clone()));
     try!(file.read_to_string(&mut content));
-    if !content.contains("libtensorflow_c.so") {
+    if content.contains("libtensorflow_c.so") {
+        log!("Build file {:?} already patched", build_file);
+    } else {
+        log!("Patching build file {:?}", build_file);
         try!(file.write_all(br#"
 cc_binary(
     name = "libtensorflow_c.so",
@@ -80,7 +98,10 @@ cc_binary(
 
 fn run<F>(name: &str, mut configure: F) where F: FnMut(&mut Command) -> &mut Command {
     let mut command = Command::new(name);
-    if !ok!(configure(&mut command).status()).success() {
-        panic!("failed to execute {:?}", command);
+    let configured = configure(&mut command);
+    log!("Executing {:?}", configured);
+    if !ok!(configured.status()).success() {
+        panic!("failed to execute {:?}", configured);
     }
+    log!("Command {:?} finished successfully", configured);
 }
