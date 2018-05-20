3155aee195e468fad15a780e2cae2b5a0ca99681
Mason Kramer <mason.kramer@gmail.com>
2018-05-20 09:24:54 -0700

    drop crc32 crate

--- src/io.rs
+++ src/io.rs
@@ -3,11 +3,10 @@
 //! See the [tensorflow docs](https://www.tensorflow.org/api_guides/python/python_io#tfrecords-format-details) for details of this format.
 
 extern crate byteorder;
-extern crate crc32c;
 
 use std::io;
 use std::io::prelude::*;
-
+use self::byteorder::{ByteOrder, LittleEndian as LE};
 use self::byteorder::WriteBytesExt;
 
 /// A type for writing bytes in the TFRecords format.
@@ -38,11 +37,11 @@ impl<W> RecordWriter<W> where W: Write {
         let mut len_bytes = vec![];
         len_bytes.write_u64::<byteorder::LittleEndian>(bytes.len() as u64)?;
 
-        let masked_len_crc32c = Self::mask(crc32c::crc32c(&len_bytes));
+        let masked_len_crc32c = Self::mask(crc32c(&len_bytes));
         let mut len_crc32c_bytes: Vec<u8> = vec![];
         len_crc32c_bytes.write_u32::<byteorder::LittleEndian>(masked_len_crc32c)?;
 
-        let masked_bytes_crc32c = Self::mask(crc32c::crc32c(&bytes));
+        let masked_bytes_crc32c = Self::mask(crc32c(&bytes));
         let mut bytes_crc32_bytes: Vec<u8> = vec![];
         bytes_crc32_bytes.write_u32::<byteorder::LittleEndian>(masked_bytes_crc32c)?;
 
@@ -58,6 +57,63 @@ impl<W> RecordWriter<W> where W: Write {
     }
 }
 
+// this pure-rust Castagnoli CRC32 algorithm is lifted from 
+// rust-snappy (https://github.com/BurntSushi/rust-snappy/blob/master/src/crc32.rs)
+// The available crc32c crates don't work on stable.
+const CASTAGNOLI_POLY: u32 = 0x82f63b78;
+
+lazy_static! {
+    static ref TABLE: [u32; 256] = make_table(CASTAGNOLI_POLY);
+    static ref TABLE16: [[u32; 256]; 16] = {
+        let mut tab = [[0; 256]; 16];
+        tab[0] = make_table(CASTAGNOLI_POLY);
+        for i in 0..256 {
+            let mut crc = tab[0][i];
+            for j in 1..16 {
+                crc = (crc >> 8) ^ tab[0][crc as u8 as usize];
+                tab[j][i] = crc;
+            }
+        }
+        tab
+    };
+}
+fn make_table(poly: u32) -> [u32; 256] {
+    let mut tab = [0; 256];
+    for i in 0u32..256u32 {
+        let mut crc = i;
+        for _ in 0..8 {
+            if crc & 1 == 1 {
+                crc = (crc >> 1) ^ poly;
+            } else {
+                crc >>= 1;
+            }
+        }
+        tab[i as usize] = crc;
+    }
+    tab
+}
+/// Returns the CRC32 checksum of `buf` using the Castagnoli polynomial.
+fn crc32c(mut buf: &[u8]) -> u32 {
+    let tab = &*TABLE;
+    let tab8 = &*TABLE16;
+    let mut crc: u32 = !0;
+    while buf.len() >= 8 {
+        crc ^= LE::read_u32(&buf[0..4]);
+        crc = tab8[0][buf[7] as usize]
+            ^ tab8[1][buf[6] as usize]
+            ^ tab8[2][buf[5] as usize]
+            ^ tab8[3][buf[4] as usize]
+            ^ tab8[4][(crc >> 24) as u8 as usize]
+            ^ tab8[5][(crc >> 16) as u8 as usize]
+            ^ tab8[6][(crc >> 8 ) as u8 as usize]
+            ^ tab8[7][(crc      ) as u8 as usize];
+        buf = &buf[8..];
+    }
+    for &b in buf {
+        crc = tab[((crc as u8) ^ b) as usize] ^ (crc >> 8);
+    }
+    !crc
+}
 ////////////////////////
 
 #[cfg(test)]
@@ -81,12 +137,12 @@ mod tests {
             record_writer.write_record("The Quick Brown Fox".as_bytes()).unwrap();
         }
 
-        let mut ef = File::open(expected_filename).unwrap();
         let mut af = File::open(actual_filename).unwrap();
-        let mut expected = vec![0; 0];
+        let mut ef = File::open(expected_filename).unwrap();
         let mut actual = vec![0; 0];
-        ef.read_to_end(&mut expected).unwrap();
+        let mut expected = vec![0; 0];
         af.read_to_end(&mut actual).unwrap();
+        ef.read_to_end(&mut expected).unwrap();
 
         assert_eq!(actual, expected);
     }
