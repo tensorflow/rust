8c89064d4ac5b9dc03228d8184acecc7e453fd00
Dmitriy Shamatrin <justnoxx@gmail.com>
2021-02-24 01:21:02 +0200

    Performed cleanup. Applied requested changes

--- examples/keras_multiple_inputs_saved_model/keras_multiple_inputs_saved_model.py
+++ examples/keras_multiple_inputs_saved_model/keras_multiple_inputs_saved_model.py
@@ -8,7 +8,6 @@ from tensorflow.keras.layers import Concatenate
 from tensorflow.keras import Model
 import numpy as np
 
-
 input = Input((5))
 x = Flatten()(input)
 x = Dense(3,'relu')(x)
@@ -28,11 +27,13 @@ model = Model(inputs=[input1, input2], outputs=dense_layer)
 v1 = np.array([[0.1, 0.2, 0.3, 0.4, 0.5]])
 v2 = np.array([[0.6, 0.7, 0.8, 0.9, 0.1]])
 print(v1.shape)
+print(v2.shape)
+
 x1, x2 = np.random.randn(100, 5), np.random.randn(100, 5)
 print(x1.shape)
+print(x2.shape)
 y = np.random.randn(100, 1)
-# print(v1.shape)
-# print(v2.shape)
+
 outputs = np.array([1.0]);
 model.compile(optimizer ='adam',loss='binary_crossentropy', metrics = ['accuracy'])
 model.fit([v1, v2], outputs, epochs=1, batch_size=1)
--- examples/keras_single_input_saved_model.rs
+++ examples/keras_single_input_saved_model.rs
@@ -4,14 +4,10 @@ fn main() {
     // In this file test_in_input is being used while in the python script,
     // that generates the saved model from Keras model it has a name "test_in".
     // For multiple inputs _input is not being appended to signature input parameter name.
-    let signature_input_1_parameter_name = "test_in1";
-    let signature_input_2_parameter_name = "test_in2";
+    let signature_input_parameter_name = "test_in_input";
     let signature_output_parameter_name = "test_out";
-
-    let save_dir = "examples/keras_multiple_inputs_saved_model";
-
-    let tensor1: Tensor<f32> = Tensor::from(&[0.1, 0.2, 0.3, 0.4, 0.5][..]);
-    let tensor2: Tensor<f32> = Tensor::from(&[0.6, 0.7, 0.8, 0.9, 0.1][..]);
+    let save_dir = "examples/keras_single_input_saved_model";
+    let tensor = Tensor::from(&[0.1, 0.2, 0.3, 0.4, 0.5][..]);
     let mut graph = Graph::new();
 
     let bundle = SavedModelBundle::load(&SessionOptions::new(), &["serve"], &mut graph, save_dir)
@@ -23,32 +19,28 @@ fn main() {
         .meta_graph_def()
         .get_signature("serving_default")
         .unwrap();
-    let input_info1 = signature
-        .get_input(signature_input_1_parameter_name)
-        .unwrap();
-    let input_info2 = signature
-        .get_input(signature_input_2_parameter_name)
-        .unwrap();
+
+    let input_info = signature.get_input(signature_input_parameter_name).unwrap();
     let output_info = signature
         .get_output(signature_output_parameter_name)
         .unwrap();
 
-    let input_op1 = graph
-        .operation_by_name_required(&input_info1.name().name)
-        .unwrap();
-    let input_op2 = graph
-        .operation_by_name_required(&input_info2.name().name)
+    let input_op = graph
+        .operation_by_name_required(&input_info.name().name)
         .unwrap();
     let output_op = graph
         .operation_by_name_required(&output_info.name().name)
         .unwrap();
+
     let mut args = SessionRunArgs::new();
-    args.add_feed(&input_op1, 0, &tensor1);
-    args.add_feed(&input_op2, 0, &tensor2);
+    args.add_feed(&input_op, 0, &tensor);
+
     let out = args.request_fetch(&output_op, 0);
+
     let result = session
         .run(&mut args)
-        .expect("Error occured during calculations: {:?}");
+        .expect("Error occurred during calculations");
     let out_res: f32 = args.fetch(out).unwrap()[0];
+
     println!("Results: {:?}", out_res);
 }
