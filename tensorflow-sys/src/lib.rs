02f092e0fc60bfb0ba180a16d90bee068476481f
dskkato <dkato@topcon.com>
2021-08-26 09:55:38 +0900

    update bindgen with eager c_api

--- tensorflow-sys/src/lib.rs
+++ tensorflow-sys/src/lib.rs
@@ -2,7 +2,8 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-include!("bindgen.rs");
+include!("c_api.rs");
+include!("eager/c_api.rs");
 
 pub use crate::TF_AttrType::*;
 pub use crate::TF_Code::*;
