1918f96fcbcb1645964086ab567ef14ca7f21b1a
dskkato <dkato@topcon.com>
2021-09-08 09:53:18 +0900

    update the link to bindgen's repository

--- tensorflow-sys/generate_bindgen_rs.sh
+++ tensorflow-sys/generate_bindgen_rs.sh
@@ -3,7 +3,7 @@
 if ! which bindgen > /dev/null; then
     echo "ERROR: Please install 'bindgen' using cargo:"
     echo "    cargo install bindgen"
-    echo "See https://github.com/servo/rust-bindgen for more information."
+    echo "See https://github.com/rust-lang/rust-bindgen for more information."
     exit 1
 fi
 
@@ -13,7 +13,7 @@ include_dir="$HOME/git/tensorflow"
 # Generate bindings for function, type, and var if its name starts with `TF_` or `TFE_`.
 allowlist="--allowlist-function TFE?_.+ --allowlist-type TFE?_.+ --allowlist-var TFE?_.+"
 
-# See https://github.com/servo/rust-bindgen/issues/550 as to why
+# See https://github.com/rust-lang/rust-bindgen/issues/550 as to why
 bindgen_options="--size_t-is-usize --default-enum-style=rust --generate-inline-functions"
 
 cmd="bindgen ${allowlist} ${bindgen_options} wrapper.h --output src/bindgen.rs -- -I ${include_dir}"
