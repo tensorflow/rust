ab8d20d58e2b30032222dc7c30e634dedf8c2edf
jerry.wang <jerry.wang@everymatrix.com>
2022-08-17 08:51:50 +0800

    Revert "include experimental C api in tensorflow-sys"
    This reverts commit 260caa7578ec04e6a6fd68d42c99644dc95af9c5.

--- tensorflow-sys/generate_bindgen_rs.sh
+++ tensorflow-sys/generate_bindgen_rs.sh
@@ -10,7 +10,7 @@ fi
 include_dir="$HOME/git/tensorflow"
 
 bindgen_options_c_api="--allowlist-function TF_.+ --allowlist-type TF_.+ --allowlist-var TF_.+ --size_t-is-usize --default-enum-style=rust --generate-inline-functions"
-cmd="bindgen ${bindgen_options_c_api} ${include_dir}/tensorflow/c/c_api_experimental.h --output src/c_api.rs --  -I ${include_dir}"
+cmd="bindgen ${bindgen_options_c_api} ${include_dir}/tensorflow/c/c_api.h --output src/c_api.rs --  -I ${include_dir}"
 echo ${cmd}
 ${cmd}
 
@@ -20,10 +20,9 @@ echo ${cmd}
 ${cmd}
 
 bindgen_options_runtime_functions="--allowlist-function TF_.+ --blocklist-type .+ --size_t-is-usize --default-enum-style=rust --generate-inline-functions"
-cmd="bindgen ${bindgen_options_runtime_functions} ${include_dir}/tensorflow/c/c_api_experimental.h --output src/runtime_linking/c_api.rs --  -I ${include_dir}"
+cmd="bindgen ${bindgen_options_runtime_functions} ${include_dir}/tensorflow/c/c_api.h --output src/runtime_linking/c_api.rs --  -I ${include_dir}"
 echo ${cmd}
 ${cmd}
-
 
 bindgen_options_runtime_types="--allowlist-type TF_.+ --blocklist-function .+ --size_t-is-usize --default-enum-style=rust --generate-inline-functions"
 cmd="bindgen ${bindgen_options_runtime_types} ${include_dir}/tensorflow/c/c_api.h --output src/runtime_linking/types.rs --  -I ${include_dir}"
@@ -32,4 +31,3 @@ ${cmd}
 
 echo "link! {\n$(cat src/runtime_linking/c_api.rs)" > src/runtime_linking/c_api.rs
 echo } >> src/runtime_linking/c_api.rs
-
