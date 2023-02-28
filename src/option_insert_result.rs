568ff56a00894152be5b10ae4525c435eb727008
Ramón García Fernández <ramon.garcia.f@gmail.com>
2023-02-28 19:29:13 +0100

    More fmt fixes

--- src/option_insert_result.rs
+++ src/option_insert_result.rs
@@ -1,12 +1,14 @@
 // Similar to Option<T>.get_or_insert_with, for a function that returns a result.
 pub trait OptionInsertWithResult<T> {
     fn get_or_insert_with_result<F, E>(&mut self, f: F) -> Result<&T, E>
-        where F: FnOnce() -> Result<T, E>;
+    where
+        F: FnOnce() -> Result<T, E>;
 }
 
 impl<T> OptionInsertWithResult<T> for Option<T> {
     fn get_or_insert_with_result<F, E>(&mut self, f: F) -> Result<&T, E>
-        where F: FnOnce() -> Result<T, E>
+    where
+        F: FnOnce() -> Result<T, E>,
     {
         if self.is_none() {
             *self = Some(f()?);
